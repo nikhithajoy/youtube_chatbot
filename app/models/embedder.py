7875097097de7a882f4633c7d046f720ee208cce
nikhithajoy <nikhitha.joy.official@gmail.com>
2025-07-15 03:37:26 +0530

    added embed model

---
+++ app/models/embedder.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+from typing import List
+
+
+class Embedder:
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        self.model = SentenceTransformer(model_name)
+    
+    def encode_texts(self, texts: List[str]) -> List[List[float]]:
+        return self.model.encode(texts, show_progress_bar=True, convert_to_numpy=True)
